f8077d0bb2ced09d6ed09acfa422e1b39a940177
Piruoiru <matteopiron2003@gmail.com>
2025-06-24 11:46:31 +0200

    rimossi commenti scheduler

--- laravelProject/routes/console.php
+++ laravelProject/routes/console.php
@@ -11,26 +11,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Schedule::call(function () {
-//     Log::info('Scheduler /analyze partito');   // debug
-
-//    var_dump(config('services.llm.url'));
-//     $response = Http::baseUrl(config('services.llm.url'))   //  ←  http(s)://tuo-sito.test
-//     ->acceptJson()
-//     ->timeout(30)
-//     ->post('/analyze');                   // usa il path reale
-    
-//     Log::info('Scheduler /analyze terminato', [
-//         'status'   => $response->status(),
-//         'body'     => $response->body(),          // o ->json()
-//     ]);
-
-//     // opzionale: se vuoi che il task fallisca su 4xx/5xx
-//     // $response->throw();
-// })
-// ->dailyAt('14:59')
-// ->timezone('Europe/Rome');
-
 
 Schedule::call(fn (ConversationApi $api) => $api->analyze())
         ->dailyAt('00:00')
